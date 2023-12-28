bb8c4f531d88217564de8e9a48dba93f8594e4f9
Manuel Enrique Castañeda Santana <mike.nike335@gmail.com>
2023-12-27 19:15:25 -0600

    Update app.component.ts

--- clientes-app2/src/app/app.component.ts
+++ clientes-app2/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Bienvenido a Angular- Spring';
   curso: string = 'Curso Spring con Angular';
-  profesor: string = 'Manuel Castañeda Santana';
+  profesor: string = 'Manuel Castañeda Santana Cambia';
 }
